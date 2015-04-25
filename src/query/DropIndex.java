223e321958a639770dc11a02d0104b8c41951ab7
Chao Tang <tctom992@gmail.com>
2015-04-24 22:17:13 -0400

    Drop index completed

--- src/query/DropIndex.java
+++ src/query/DropIndex.java
@@ -1,29 +1,35 @@
 package query;
 
+import global.Minibase;
+import index.HashIndex;
 import parser.AST_DropIndex;
 
 /**
  * Execution plan for dropping indexes.
  */
 class DropIndex implements Plan {
+	protected String indexName;
 
-  /**
-   * Optimizes the plan, given the parsed query.
-   * 
-   * @throws QueryException if index doesn't exist
-   */
-  public DropIndex(AST_DropIndex tree) throws QueryException {
+	/**
+	 * Optimizes the plan, given the parsed query.
+	 * 
+	 * @throws QueryException
+	 *             if index doesn't exist
+	 */
+	public DropIndex(AST_DropIndex tree) throws QueryException {
+		indexName = tree.getFileName();
+		// check if there exists such a index
+		QueryCheck.indexExists(indexName);
+	} // public DropIndex(AST_DropIndex tree) throws QueryException
 
-  } // public DropIndex(AST_DropIndex tree) throws QueryException
-
-  /**
-   * Executes the plan and prints applicable output.
-   */
-  public void execute() {
-
-    // print the output message
-    System.out.println("(Not implemented)");
-
-  } // public void execute()
+	/**
+	 * Executes the plan and prints applicable output.
+	 */
+	public void execute() {
+		new HashIndex(indexName).deleteFile();
+		Minibase.SystemCatalog.dropIndex(indexName);
+		// print the output message
+		System.out.println("Index " + indexName + "dropped.");
+	} // public void execute()
 
 } // class DropIndex implements Plan
