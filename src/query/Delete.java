1e9dd2cbe73854da55eb682519b388c30aa55242
Chao Tang <tctom992@gmail.com>
2015-04-26 19:09:53 -0400

    Combined all part two, and did some testing

--- src/query/Delete.java
+++ src/query/Delete.java
@@ -1,7 +1,16 @@
 package query;
 
-import parser.AST_Delete;
+import java.util.ArrayList;
 
+import global.Minibase;
+import global.RID;
+import global.SearchKey;
+import parser.AST_Delete;
+import index.HashIndex;
+import global.*;
+import heap.HeapFile;
+import parser.AST_Insert;
+import relop.*;
 /**
  * Execution plan for deleting tuples.
  */
@@ -11,18 +20,80 @@ class Delete implements Plan {
    * Optimizes the plan, given the parsed query.
    * 
    * @throws QueryException if table doesn't exist or predicates are invalid
+   * 
    */
-  public Delete(AST_Delete tree) throws QueryException {
-
+	String file;
+	Schema schema;
+	Predicate[][] preds;
+  
+	public Delete(AST_Delete tree) throws QueryException {
+		file = tree.getFileName();
+		preds = tree.getPredicates();
+		schema = Minibase.SystemCatalog.getSchema(file);
+		QueryCheck.tableExists(file);
+		QueryCheck.predicates(schema, preds);
   } // public Delete(AST_Delete tree) throws QueryException
 
   /**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
+	  	HeapFile f = new HeapFile(file);
+	  	FileScan fs = new FileScan(schema,f);
+	  	ArrayList<RID> delIDs = new ArrayList<RID>();
+	  	ArrayList<Tuple> tuples = new ArrayList<Tuple>();
+	  	while(fs.hasNext()){ //consider pred in same list as AND 
+	  		Tuple t = fs.getNext();
+	  		boolean pass = true;
+	  		for(int i=0; i<preds.length;i++){
+	  			boolean localPass = false;
+	  			for(Predicate p : preds[i]){
+	  				localPass |= p.evaluate(t);
+	  			}
+	  			pass &= localPass;	
+	  		}
+	  		
+	  		if(pass || preds.length==0){//tuple match
+	  			delIDs.add(fs.getLastRID());	
+	  			tuples.add(t);
+	  		}
+	  	}
+	  	fs.close();
+	  	for(int i=0;i<tuples.size();i++){ 
+	  		
+	  		
+	  		//RID rid = f.insertRecord(tuple.getData());
+	  		RID rid = delIDs.get(i);
+	  		Tuple tuple = tuples.get(i);
+	  		// update indexes of this table
+	  		IndexDesc[] indexes = Minibase.SystemCatalog.getIndexes(file);
+	  		for (IndexDesc ind : indexes)
+	  		{
+				String indexName = ind.indexName;
+				String colName = ind.columnName;
+				HashIndex index = new HashIndex(indexName);
+				SearchKey key = new SearchKey(tuple.getField(colName));
+				index.deleteEntry(key, rid);
+			}
+	  		
+	  		f.deleteRecord(rid);
+	  		// print the output message
+	  		System.out.println("1 rows deleted.");
+	  	}
+	  	
+	    /*fs = new FileScan(schema,f);
+	    while(fs.hasNext()){//????????? why this shit can has next after i delete all.
+	    	Tuple t = fs.getNext();
+	    	Object[] allF = t.getAllFields();
+	    	System.out.print((Integer)allF[0]+",");
+	    	System.out.print((String)allF[1]+",");
+	    	System.out.print((Float)allF[2]+",");
+	    	System.out.println();
+	    }*/
+	    fs.close();
 
     // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+    //System.out.println("0 rows affected. (Not implemented)");
 
   } // public void execute()
 
--- src/query/Update.java
+++ src/query/Update.java
@@ -1,29 +1,107 @@
 package query;
 
+import index.HashIndex;
+
+import java.util.ArrayList;
+
+import global.AttrType;
+import global.Minibase;
+import global.RID;
+import global.SearchKey;
+import heap.HeapFile;
 import parser.AST_Update;
+import relop.FileScan;
+import relop.Predicate;
+import relop.Schema;
+import relop.Tuple;
 
 /**
  * Execution plan for updating tuples.
  */
 class Update implements Plan {
 
-  /**
-   * Optimizes the plan, given the parsed query.
-   * 
-   * @throws QueryException if invalid column names, values, or pedicates
-   */
-  public Update(AST_Update tree) throws QueryException {
+	private String file;
+	private Schema schema;
+	private Predicate[][] preds;
+	private Object[] values;
+	private String[] columns;
 
-  } // public Update(AST_Update tree) throws QueryException
+	/**
+	 * Optimizes the plan, given the parsed query.
+	 * 
+	 * @throws QueryException
+	 *             if invalid column names, values, or pedicates
+	 */
+	public Update(AST_Update tree) throws QueryException {
+		file = tree.getFileName();
+		preds = tree.getPredicates();
+		schema = Minibase.SystemCatalog.getSchema(file);
+		values = tree.getValues();
+		columns = tree.getColumns();
+		// check columns
+		for (String c : columns) {
+			QueryCheck.columnExists(schema, c);
+		}
+		// check value class
+		int[] fn = new int[values.length];
+		for (int i = 0; i < values.length; i++) {
+			fn[i] = schema.fieldNumber(columns[i]);
+		}
+		QueryCheck.updateValues(schema, fn, values);
 
-  /**
-   * Executes the plan and prints applicable output.
-   */
-  public void execute() {
+		QueryCheck.tableExists(file);
+		QueryCheck.predicates(schema, preds);
 
-    // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+	} // public Update(AST_Update tree) throws QueryException
 
-  } // public void execute()
+	/**
+	 * Executes the plan and prints applicable output.
+	 */
+	public void execute() {
+
+		HeapFile f = new HeapFile(file);
+		FileScan fs = new FileScan(schema, f);
+		while (fs.hasNext()) { // consider pred in same list as AND
+			Tuple t = fs.getNext();
+			boolean pass = true;
+			for (int i = 0; i < preds.length; i++) {
+				boolean localPass = false;
+				for (Predicate p : preds[i]) {
+					localPass |= p.evaluate(t);
+				}
+				pass &= localPass;
+			}
+
+			if (pass || preds.length == 0) {// tuple match
+				Tuple updateT = new Tuple(schema, t.getData());
+				for (int i = 0; i < columns.length; i++) {
+					updateT.setField(columns[i], values[i]);
+				}
+
+				IndexDesc[] indexes = Minibase.SystemCatalog.getIndexes(file);
+				for (IndexDesc ind : indexes) {
+					String indexName = ind.indexName;
+					String colName = ind.columnName;
+					HashIndex index = new HashIndex(indexName);
+					SearchKey oldKey = new SearchKey(t.getField(colName));
+					SearchKey newKey = new SearchKey(updateT.getField(colName));
+					index.deleteEntry(oldKey, fs.getLastRID());
+					index.insertEntry(newKey, fs.getLastRID());
+				}
+
+				f.updateRecord(fs.getLastRID(), updateT.getData());
+			}
+		}
+		fs.close();
+		/*
+		 * fs = new FileScan(schema,f); while(fs.hasNext()){ Tuple t =
+		 * fs.getNext(); Object[] allF = t.getAllFields();
+		 * System.out.print((Integer)allF[0]+",");
+		 * System.out.print((String)allF[1]+",");
+		 * System.out.print((Float)allF[2]+","); System.out.println(); }
+		 */
+
+		// public void execute()
+	} // public void execute()
 
 } // class Update implements Plan
